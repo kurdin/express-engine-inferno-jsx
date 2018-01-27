b784b9d67eaad667a9caa4acdfb5e61b52231ee1
Sergey <skurdin@yahoo.com>
2018-01-27 14:22:01 -0500

    Update appshared.js

--- appshared.js
+++ appshared.js
@@ -1,17 +1,3 @@
-var Inferno = require('inferno');
-var createVNode = Inferno.createVNode;
-
-/* jsx original 
 var AppSharedData = ({shared}) => shared && <script dangerouslySetInnerHTML={{__html: 'window.__clientAppShared = ' + JSON.stringify(shared).replace(/</g, '\\u003c').replace(/-->/g, '--\\>').replace(/\u2028/g, '\\u2028').replace(/\u2029/g, '\\u2029') + ';' + '(function() { if (typeof window != "undefined" && window.__clientAppShared && !window.appShared) window.appShared = window.__clientAppShared; })();'}}/>;
-*/
-
-var AppSharedData = function AppSharedData(_ref2) {
-  var shared = _ref2.shared;
-  return shared && createVNode(2, "script", null, null, {
-    "dangerouslySetInnerHTML": {
-      __html: 'window.__clientAppShared = ' + JSON.stringify(shared).replace(/</g, "\\u003c").replace(/-->/g, '--\\>').replace(/\u2028/g, "\\u2028").replace(/\u2029/g, "\\u2029") + ';' + '(function() { if (typeof window != "undefined" && window.__clientAppShared && !window.appShared) window.appShared = window.__clientAppShared; })();'
-    }
-  });
-};
 
 module.exports = AppSharedData;
